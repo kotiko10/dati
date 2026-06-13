056627d4cb311025f1c4acd96663ed07d3ac755a
kotiko <koti1.nebieridze@gmail.com>
2026-06-13 14:33:47 +0200

    responsive for phone

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "../styles/navbar.css";
 
 function Navbar() {
+   const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="logo">DA TI</div>
 
-      <div className="nav-links">
-        <Link to="/dati">Home</Link>
-        <Link to="tour">Tour</Link>
-        <Link to="music">Music</Link>
-        <Link to="merch">Merch</Link>
-        <Link to="socials">Find Us</Link>
+       <div className="hamburger" onClick={() => setOpen(!open)}>
+        ☰
+      </div>
+
+     <div className={`nav-links ${open ? "open" : ""}`}>
+         <Link to="/dati" onClick={() => setOpen(false)}>Home</Link>
+        <Link to="/tour" onClick={() => setOpen(false)}>Tour</Link>
+        <Link to="/music" onClick={() => setOpen(false)}>Music</Link>
+        <Link to="/merch" onClick={() => setOpen(false)}>Merch</Link>
+        <Link to="/socials" onClick={() => setOpen(false)}>Find Us</Link>
       </div>
     </nav>
   );
